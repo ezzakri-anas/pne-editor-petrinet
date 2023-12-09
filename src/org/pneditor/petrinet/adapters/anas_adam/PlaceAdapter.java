8614c5d3afed7fdb75dce767c264145b4ee91f0e
ezzakri-anas <ezzakrianas65@gmaiL.com>
2023-12-09 16:52:51 +0100

    Implemented place and arc adapters

--- src/org/pneditor/petrinet/adapters/anas_adam/PlaceAdapter.java
+++ src/org/pneditor/petrinet/adapters/anas_adam/PlaceAdapter.java
@@ -1,27 +1,41 @@
 package org.pneditor.petrinet.adapters.anas_adam;
 
-public class PlaceAdapter {
+import org.pneditor.petrinet.AbstractPlace;
+import org.pneditor.petrinet.models.anas_adam.Place;
 
-	public PlaceAdapter() {
-		
+public class PlaceAdapter extends AbstractPlace{
+	private Place model_place;
+
+	public PlaceAdapter(final String label) {
+		super(label);
+		this.model_place = new Place(0);
 	}
 	
+	public Place getModelPlace() {
+		return this.model_place;
+	}
+	
+	@Override
 	public void addToken() {
 		this.setTokens(this.getTokens() + 1);
 	}
 
+	@Override
 	public void removeToken() {
-		removeTokenNbre(1);
+		this.model_place.removeTokenNbre(1);
 	}
 
+	@Override
 	public int getTokens() {
-		return getTokenNbre();
-	}
-
-	public void setTokens(int tokens) {
-		setTokenNbre(tokens)
+		return this.model_place.getTokenNbre();
 	}
 	
+	@Override
+	public void setTokens(int tokens) {
+		this.model_place.setTokenNbre(tokens);
+	}
+	
+	@Override
 	public boolean isPlace() {
 		return true;
 	}
---
+++ src/org/pneditor/petrinet/adapters/anas_adam/TransitionAdapter.java
@@ -0,0 +1,16 @@
+package org.pneditor.petrinet.adapters.anas_adam;
+
+import org.pneditor.petrinet.models.anas_adam.Transition;
+
+public class TransitionAdapter {
+	private Transition model_transition;
+
+	public TransitionAdapter() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Transition getModelTransition() {
+		return this.model_transition;
+	}
+
+}
